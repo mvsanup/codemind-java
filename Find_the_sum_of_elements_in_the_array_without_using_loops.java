5999453a37c028ccba77370438f884400026a30c
mvsanup <97591995+mvsanup@users.noreply.github.com>
2022-09-14 11:22:12 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<n;i++)         {             c=c+a[i];         }         System.out.println(c);     } }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c=c+a[i];
+        }
+        System.out.println(c);
+    }
+}
