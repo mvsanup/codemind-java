95151de569eb11c7a76104ad2637962222176e74
mvsanup <97591995+mvsanup@users.noreply.github.com>
2022-10-08 09:06:57 +0530

    import java.util.*; class des {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=0;         int [] a=new int[50];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(a[i]<a[i+1])             {                 c+=1;             }         }         if(c==n-1)         {             System.out.println("yes");         }         else             System.out.println("no");     } }

---
+++ Ascending_array.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+class des
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        int [] a=new int[50];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]<a[i+1])
+            {
+                c+=1;
+            }
+        }
+        if(c==n-1)
+        {
+            System.out.println("yes");
+        }
+        else
+            System.out.println("no");
+    }
+}
