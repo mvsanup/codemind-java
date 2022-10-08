12a9e1a5cdf2fd215d7fc43b7341c7857398c4cc
mvsanup <97591995+mvsanup@users.noreply.github.com>
2022-10-08 16:10:39 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in); 		String a=sc.nextLine(); 		System.out.println("Hello Technicalhub"); 		System.out.print(a);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		System.out.println("Hello Technicalhub");
+		System.out.print(a);
+    }
+}
