7c1a35fe2e7b06c0196c131361a7b78ec8de30a7
mvsanup <97591995+mvsanup@users.noreply.github.com>
2022-10-08 09:58:48 +0530

    import java.util.*; class sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int [] a=new int [n];         int s=0;         for (int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             int c=0;             for(int j=0;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                     if(c==1)                     {                         if(a[i]%2==0)                         {                             s+=a[i];                         }                     }                     if(j > i)                         a[j] = 0;                 }             }         }         System.out.println(s);     } }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] a=new int [n];
+        int s=0;
+        for (int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                    if(c==1)
+                    {
+                        if(a[i]%2==0)
+                        {
+                            s+=a[i];
+                        }
+                    }
+                    if(j > i)
+                        a[j] = 0;
+                }
+            }
+        }
+        System.out.println(s);
+    }
+}
