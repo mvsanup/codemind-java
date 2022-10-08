f90e1238d15d7174da714faf281eb03d6cc0d28b
mvsanup <97591995+mvsanup@users.noreply.github.com>
2022-10-08 16:22:25 +0530

    import java.util.*; class simple {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int si;         int p=sc.nextInt();         int t=sc.nextInt();         int r=sc.nextInt();         si=p*t*r/100;         System.out.println(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class simple
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int si;
+        int p=sc.nextInt();
+        int t=sc.nextInt();
+        int r=sc.nextInt();
+        si=p*t*r/100;
+        System.out.println(si);
+    }
+}
