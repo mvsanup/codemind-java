e1a8956ebdea7aa5e9697a8e62eeb28ee6910cdc
mvsanup <97591995+mvsanup@users.noreply.github.com>
2022-10-08 13:03:22 +0530

    import java.util.Scanner; class Pattern{     public static void main(String[] args){         Scanner in=new Scanner(System.in);         int n=in.nextInt();         for(int i=0;i<n;i++){             for(int j=n;j>0;j--){                 System.out.printf("%d ",j);             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,15 +1,12 @@
-import java.util.*;
-class pat
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int m=sc.nextInt();
-        for(int i=m;i>=1;i--)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j);
+
+import java.util.Scanner;
+class Pattern{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=n;j>0;j--){
+                System.out.printf("%d ",j);
             }
             System.out.println();
         }
